b1fffb8c829ee8f712b10ad5751273f598854485
Adarsh <adarsh2858@gmail.com>
2023-12-11 17:33:16 +0530

    generate proto buf files and dependencies tracking using go mod and add server side logic

---
+++ server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/adarsh2858/grpc-usecases/proto"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":3000"
+)
+
+type helloWorld struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	l, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Something went wrong;\n %v", err)
+	}
+
+	g := grpc.NewServer()
+	pb.RegisterGreetServiceServer(g, &helloWorld{})
+	log.Printf("Server started at %v", l.Addr())
+
+	if err := g.Serve(l); err != nil {
+		log.Fatalf("grpc server could not be served;\n %v", err)
+	}
+}
